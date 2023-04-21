ec3ceab73f0a411273d1a36c8391fb8dbdbdff4a
javi <javi3lopez@gmail.com>
2023-04-21 15:51:38 -0300

    viernes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,84 @@
+class marco {
+  constructor(nombreCliente, largo, ancho, molduraElegida, precioRedondeado) {
+    this.nombreCliente = nombreCliente,
+      this.largo = largo,
+      this.ancho = ancho,
+      this.molduraElegida = molduraElegida,
+      this.costoVidrio = costoVidrio,
+      this.perimetro = perimetro,
+      this.precioRedondeado = precioRedondeado;
+  }
+}
+
+const marcos = []
+
+if (localStorage.getItem('marcos')) {
+  let marco = JSON.parse(localStorage.getItem('marcos'));
+
+  for (let i = 0; i < marcos.length; i++) {
+    marcos.push(marco[i]);
+  }
+}
 // inputs
 
-const inputNombre = document.querySelector("#inputNombre");
-const largo = parseFloat(document.querySelector("#inputLargo").value);
-const ancho = parseFloat(document.querySelector("#inputAncho").value);
-const inputMoldura = document.querySelector("#select-moldura");
-const inputVidrio = document.querySelector("#inputVidrio");
+const formulario = document.getElementById('formulario');
 const btnCalcular = document.querySelector("#btnCalcular");
 const fragment = document.createDocumentFragment();
+
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  calc();
+  verPresupuesto();
+})
+
+function calc() {
+
+  const nombreCliente = document.querySelector("#inputNombre").value;
+  const largo = parseFloat(document.querySelector("#inputLargo").value);
+  const ancho = parseFloat(document.querySelector("#inputAncho").value);
+  const molduraElegida = (document.querySelector("#inputMoldura").value);
+  const costoVidrio = parseFloat(ancho * largo / 5.6);
+  const perimetro = parseFloat(2 * ancho + 2 * largo);
+  let costoMoldura;
+
+  if (perimetro < 244) {
+    costoMoldura = (molduraElegida.value);
+  } else if (perimetro >= 244 && perimetro <= 488) {
+    costoMoldura = (2 * molduraElegida.value);
+  } else
+  costoMoldura = (3 * molduraElegida.value);
+
+  let costoEstimado = costoVidrio + costoMoldura + 100;
+  let precioFinal = parseInt(2 * costoEstimado);
+  let precioRedondeado = Math.ceil(precioFinal / 50) * 50;
+
+
+  const nuevoMarco = new marco(nombreCliente, largo, ancho, molduraElegida.nombre, precioRedondeado);
+  marcos.push(nuevoMarco);
+  //Agrego al LocalStorage:
+  localStorage.setItem('marcos', JSON.stringify(marcos));
+  formulario.reset();
+}
+
+
 const contenedor = document.querySelector("#contenedor");
+
+function verPresupuesto() {
+  contenedor.innerHTML = "";
+  marcos.forEach((marco) => {
+    const div = document.createElement(`div`);
+    div.innerHTML = ` 
+    
+    <div class="card m-2 p-5 ">
+      <p>Nuevo marco: ${marco.nombreCliente}   </p>
+      <p>medidas: ${marco.largo} x ${marco.ancho}</p>
+      <p>moldura: ${marco.molduraElegida}</p>
+      <p>precio: ${marco.precioRedondeado}</p>
+    </div>  `;
+
+     contenedor.appendChild(div);
+})}
 
 // variantes de molduras
 
@@ -30,70 +101,3 @@ let costoVidrio = 0;
 let perimetro = 0;
 let costoMoldura = 0;
 let precioRedondeado = 0;
-
-// cálculos
-
-function calc(largo, ancho) {
-
-const costoVidrio = parseFloat(ancho * largo / 5.6)
-const perimetro = parseFloat(2 * ancho + 2 * largo)
-let costoMoldura;
-
-
-if (perimetro < 244) {
-  costoMoldura = inputMoldura.value
-} else if (perimetro >= 244 && perimetro <= 488) {
-  costoMoldura = (2 * inputMoldura.value);
-} else
-  costoMoldura = 3 * inputMoldura.value;
-
-let costoEstimado = costoVidrio + costoMoldura+100
-let precioFinal = parseInt(2 * costoEstimado)
-let precioRedondeado = Math.ceil(precioFinal / 50) * 50;
-
-}
-
-
-const marcos = []
-
-// objeto constructor de marcos
-
-class marco {
-  constructor(nombre, largo, ancho, molduraElegida, costoVidrio, perimetro, precioRedondeado) {
-    this.nombre = nombre,
-      this.largo = largo,
-      this.ancho = ancho,
-      this.molduraElegida = molduraElegida,
-      this.costoVidrio = costoVidrio,
-      this.perimetro = perimetro,
-      this.precioRedondeado = precioRedondeado
-  }
-}
-
-btnCalcular.addEventListener("click", () => {
-  console.log(largo, ancho, inputMoldura);
-})
-// botón para crear marco nuevo
-
-/* btnCalcular.addEventListener("click", () => {
-  
-  console.log(largo, ancho, inputMoldura.value);
-  calc()
-
-  const newMarco = new marco(
-    inputNombre.value,
-    largo.value,
-    ancho.value,
-    inputMoldura.nombre,
-    costoVidrio,
-    perimetro,
-    precioRedondeado,
-  );
-
-  console.log(newMarco);
-  productos.push(newMarco);
-
-  console.log(marcos);
-  console.log(inputMoldura.value);
-
-}); */
