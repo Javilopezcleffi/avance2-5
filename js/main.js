05575a5d5999e74242eca54e6e3236771c298c32
Javilopezcleffi <124194618+Javilopezcleffi@users.noreply.github.com>
2023-04-24 15:27:13 -0300

    lunes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
+
+let costoVidrio = 0;
+let perimetro = 0;
+let costoMoldura = 0;
+let precioRedondeado = 0;
+
+
 class marco {
-  constructor(nombreCliente, largo, ancho, molduraElegida, precioRedondeado) {
+  constructor(nombreCliente, largo, ancho, molduraSeleccionada, costoMoldura, precioRedondeado) {
     this.nombreCliente = nombreCliente,
       this.largo = largo,
       this.ancho = ancho,
-      this.molduraElegida = molduraElegida,
+      this.molduraSeleccionada = molduraSeleccionada,
       this.costoVidrio = costoVidrio,
       this.perimetro = perimetro,
       this.precioRedondeado = precioRedondeado;
@@ -19,7 +26,6 @@ if (localStorage.getItem('marcos')) {
     marcos.push(marco[i]);
   }
 }
-// inputs
 
 const formulario = document.getElementById('formulario');
 const btnCalcular = document.querySelector("#btnCalcular");
@@ -37,24 +43,26 @@ function calc() {
   const nombreCliente = document.querySelector("#inputNombre").value;
   const largo = parseFloat(document.querySelector("#inputLargo").value);
   const ancho = parseFloat(document.querySelector("#inputAncho").value);
-  const molduraElegida = (document.querySelector("#inputMoldura").value);
   const costoVidrio = parseFloat(ancho * largo / 5.6);
   const perimetro = parseFloat(2 * ancho + 2 * largo);
   let costoMoldura;
+  const molduraIndex = document.querySelector("#inputMoldura").selectedIndex - 1;
+  const molduraSeleccionada = molduras[molduraIndex];
 
-  if (perimetro < 244) {
-    costoMoldura = (molduraElegida.value);
-  } else if (perimetro >= 244 && perimetro <= 488) {
-    costoMoldura = (2 * molduraElegida.value);
-  } else
-  costoMoldura = (3 * molduraElegida.value);
+if (perimetro < 244) {
+  costoMoldura = molduraSeleccionada.precio;
+} else if (perimetro >= 244 && perimetro <= 488) {
+  costoMoldura = 2 * molduraSeleccionada.precio;
+} else {
+  costoMoldura = 3 * molduraSeleccionada.precio;
+}
 
-  let costoEstimado = costoVidrio + costoMoldura + 100;
+  let costoEstimado = (costoVidrio + costoMoldura + 100);
   let precioFinal = parseInt(2 * costoEstimado);
   let precioRedondeado = Math.ceil(precioFinal / 50) * 50;
 
 
-  const nuevoMarco = new marco(nombreCliente, largo, ancho, molduraElegida.nombre, precioRedondeado);
+  const nuevoMarco = new marco(nombreCliente, largo, ancho, molduraSeleccionada, costoVidrio, perimetro, costoMoldura, precioRedondeado);
   marcos.push(nuevoMarco);
   //Agrego al LocalStorage:
   localStorage.setItem('marcos', JSON.stringify(marcos));
@@ -73,11 +81,12 @@ function verPresupuesto() {
     <div class="card m-2 p-5 ">
       <p>Nuevo marco: ${marco.nombreCliente}   </p>
       <p>medidas: ${marco.largo} x ${marco.ancho}</p>
-      <p>moldura: ${marco.molduraElegida}</p>
+      <p>moldura: ${marco.molduraSeleccionada.nombre}</p>
       <p>precio: ${marco.precioRedondeado}</p>
     </div>  `;
 
      contenedor.appendChild(div);
+     
 })}
 
 // variantes de molduras
@@ -96,8 +105,3 @@ for (const moldura of molduras) {
   fragment.appendChild(option);
 }
 inputMoldura.appendChild(fragment);
-
-let costoVidrio = 0;
-let perimetro = 0;
-let costoMoldura = 0;
-let precioRedondeado = 0;
